06f7ff69933f7d0acec5151ae11e4eeb9cc32e3f
jinayang15 <jinayang15@gmail.com>
2022-05-19 23:03:49 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Does it work?");
+		}
 
 	}
 
