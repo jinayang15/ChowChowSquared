8f073539dcfe931b03bee25e21f9f89783b6fc78
jinayang15 <jinayang15@gmail.com>
2022-05-19 23:19:54 -0400

    hello

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Does it work?");
-		}
+		System.out.println("hello");
 
 	}
 
