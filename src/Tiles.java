9eaa27e7083c2a457cbac7543d9c816eb760fc8b
jinayang15 <jinayang15@gmail.com>
2022-05-21 20:44:54 -0400

    Added Tile class with solid instance variable. Tile belongs to Rectangle class so that we can use intersect method.

---
+++ src/Tiles.java
@@ -0,0 +1,16 @@
+import java.awt.Rectangle;
+
+public class Tiles extends Rectangle {
+	// if solid = true, you cannot pass thru the block
+	// if solid = false, you can pass thru the block
+	private boolean solid = true;
+	// check if Tile is solid
+	// returns boolean
+	public boolean checkSolid() {
+		return this.solid;
+	}
+	// set Tile to solid or not solid with boolean
+	public void setSolidity(boolean solidity) {
+		this.solid = solidity;
+	}
+}
