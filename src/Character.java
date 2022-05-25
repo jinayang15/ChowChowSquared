e885c5f5136a2588e0e1ea95818efd81b0ab94f7
jinayang15 <jinayang15@gmail.com>
2022-05-25 09:46:12 -0400

    replaced stuff with variables + added some comments

--- src/Character.java
+++ src/Character.java
@@ -6,17 +6,18 @@ public class Character extends Rectangle {
 	// Class Variables
 	private static boolean left = false;
 	private static boolean right = false;
+	// pixels moved left and right
 	private static int moveX = 10;
 	
 	// jump = true, character is jumping
 	// jump = false, character is not jumping
 	private static boolean jump = false;
-	// how many pixels the character will move down
-	// in the air
+	// how high the character will jump and fall
 	private static int gravity = 60;
-	// -1 for up
-	// 1 for down
+	// direction determines whether the character is jumping up or falling down
+	// -1 max jump -> 1 max fall
 	private static double direction = -1;
+	// how fast the character will jump or fall
 	private static double jumpSpeed = 0.3;
 	private static double fallSpeed = 0.1;
 	
@@ -30,7 +31,7 @@ public class Character extends Rectangle {
 	}
 	
 	// movement is currently incomplete, have not considered tiles yet
-	// basic mechanics are down, maybe improve jump animation
+	// basic mechanics are down
 	// check if character is jumping
 	public boolean isJumping() {
 		return jump;
@@ -44,16 +45,18 @@ public class Character extends Rectangle {
 	public void jump() {
 		if (isJumping()) {		
 			this.translate(0, (int) (gravity*direction));
-			if(direction < 0) {
-				direction += jumpSpeed;
+			if (direction < 1) {
+				if(direction < 0) {
+					direction += jumpSpeed;
+				}
+				else {
+					direction += fallSpeed;
+				}
 			}
-			else {
-				direction += fallSpeed;
-			}
-			if (this.getY() >= 360) {
+			if (this.getY() >= Main.winHeight - Main.tileSize) {
 				direction = -1;
 				setJumping(false);
-				this.setLocation((int) this.getX(), 360);
+				this.setLocation((int) this.getX(), Main.winHeight - Main.tileSize);
 			}
 		}
 	}
@@ -83,8 +86,8 @@ public class Character extends Rectangle {
 	public void moveRight() {
 		if (this.isRight()) {
 			this.translate(moveX, 0);
-			if (this.getX() >= 480) {
-				this.setLocation(480, (int) this.getY());
+			if (this.getX() >= Main.winWidth - Main.tileSize) {
+				this.setLocation(Main.winWidth - Main.tileSize, (int) this.getY());
 			}
 		}
 	}
