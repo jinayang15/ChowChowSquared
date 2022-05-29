87661874aebcaad0a449df7aa03d00ea6c7f4e5e
KUMA <KUMA@DESKTOP-C5R9P2F>
2022-05-29 00:11:01 -0400

    imported dog sprite images

--- src/Animations.java
+++ src/Animations.java
@@ -1,3 +1,18 @@
-public class Animations {
+import java.awt.Graphics2D;
 
+public class Animations {
+	
+	public static String direction = "right";
+	
+	public void draw(Graphics2D g2) {
+		BufferedImage img = null;
+		if (direction.equals("right")) {
+			if (jump!) {
+				for (int i = 0; i < rightWalkDog1.length; i++) {
+					g2.draw(rightWalkDog1[i], main.x, main.y, 40, 40);
+				}
+			}
+		}
+	}
+	// idk what im doing
 }
--- src/Images.java
+++ src/Images.java
@@ -4,23 +4,52 @@ import java.io.File;
 import java.io.IOException;
 public class Images {
 	// anything starting with "pH" is a placeholder, not final
-	public static final BufferedImage[] dogSprites = new BufferedImage[5];
+	public static final BufferedImage[] rightWalkDog1 = new BufferedImage[6];
+	public static final BufferedImage[] rightJumpDog1 = new BufferedImage[4];
+	public static final BufferedImage[] rightIdleDog1 = new BufferedImage[9];
+	public static final BufferedImage[] lefttWalkDog1 = new BufferedImage[6];
+	public static final BufferedImage[] leftJumpDog1 = new BufferedImage[4];
+	public static final BufferedImage[] leftIdleDog1 = new BufferedImage[9];
 	public static final BufferedImage[] dirtTiles = new BufferedImage[5];
 	public static final BufferedImage[] grassTiles = new BufferedImage[2];
 	public static BufferedImage menu;
-	
+	public static BufferedImage dogRight1;
+	public static BufferedImage dogLeft1;
 	public static BufferedImage pHDog;
 	public static BufferedImage pHTile;
 	public static BufferedImage pHBG;
-	
 	
 	public static void importImage() throws IOException {
 		menu = ImageIO.read(new File("chowchowmenu.png"));
 		pHTile = ImageIO.read(new File("dirt1.png"));
 		pHDog = ImageIO.read(new File("pHChar.png"));
 		pHBG = ImageIO.read(new File("scrollBackground.png"));
+		dogRight1 = ImageIO.read(new File("whitedogright.png"));
+		dogLeft1 = ImageIO.read(new File("whitedogleft.png"));
 		for (int i = 0; i < grassTiles.length; i++) {
 			grassTiles[i] = ImageIO.read(new File("grass" + (i+1) + ".png"));
 		}
+		for (int i = rightWalkDog1.length;i >= 0; i--) {
+			rightWalkDog1[i] = grabImage(i+9,2,dogRight1);
+		}
+		for (int i = rightJumpDog1.length;i >= 0; i--) {
+			rightJumpDog1[i] = grabImage(i+9,1,dogRight1);
+		}
+		for (int i = rightIdleDog1.length;i >= 0; i--) {
+			rightIdleDog1[i] = grabImage(i,1,dogRight1);
+		}
+		for (int i = 0; i < rightWalkDog1.length; i++) {
+			lefttWalkDog1[i] = grabImage(i,2,dogLeft1);
+		}
+		for (int i = 0; i < rightJumpDog1.length; i++) {
+			leftJumpDog1[i] = grabImage(i,1,dogLeft1);
+		}
+		for (int i = 0; i < rightIdleDog1.length; i++) {
+			leftIdleDog1[i] = grabImage(i+4,1,dogLeft1);
+		}
+	}
+	public static BufferedImage grabImage(int col, int row, BufferedImage img) {
+		BufferedImage image = img.getSubimage(col*40, row*40, 40, 40);
+		return image;
 	}
 }
