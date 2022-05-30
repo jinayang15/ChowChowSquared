9ecfc5e0046f761462b056170f6954998e2e652d
jinayang15 <jinayang15@gmail.com>
2022-05-30 13:26:05 -0400

    added comments

--- src/Animations.java
+++ src/Animations.java
@@ -1,113 +1,18 @@
-import java.awt.Frame;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.awt.Graphics2D;
 
 public class Animations {
-
-    private int frameCount;                 // Counts ticks for change
-    private int frameDelay;                 // frame delay 1-12 (You will have to play around with this)
-    private int currentFrame;               // animations current frame
-    private int animationDirection;         // animation direction (i.e counting forward or backward)
-    private int totalFrames;                // total amount of frames for your animation
-
-    private boolean stopped;                // has animations stopped
-
-    private List<Frame> frames = new ArrayList<Frame>();    // Arraylist of frames 
-
-    public Animations(BufferedImage[] frames, int frameDelay) {
-        this.frameDelay = frameDelay;
-        this.stopped = true;
-
-        for (int i = 0; i < frames.length; i++) {
-            addFrame(frames[i], frameDelay);
-        }
-
-        this.frameCount = 0;
-        this.frameDelay = frameDelay;
-        this.currentFrame = 0;
-        this.animationDirection = 1;
-        this.totalFrames = this.frames.size();
-
-    }
-
-    public void start() {
-        if (!stopped) {
-            return;
-        }
-
-        if (frames.size() == 0) {
-            return;
-        }
-
-        stopped = false;
-    }
-
-    public void stop() {
-        if (frames.size() == 0) {
-            return;
-        }
-
-        stopped = true;
-    }
-
-    public void restart() {
-        if (frames.size() == 0) {
-            return;
-        }
-
-        stopped = false;
-        currentFrame = 0;
-    }
-
-    public void reset() {
-        this.stopped = true;
-        this.frameCount = 0;
-        this.currentFrame = 0;
-    }
-
-    private void addFrame(BufferedImage frame, int duration) {
-        if (duration <= 0) {
-            System.err.println("Invalid duration: " + duration);
-            throw new RuntimeException("Invalid duration: " + duration);
-        }
-
-        frames.add(new Frame());
-        currentFrame = 0;
-    }
-
-    public Frame[] getSprite() {
-        frames.get(currentFrame);
-		return Frame.getFrames();
-    }
-
-    public void update() {
-        if (!stopped) {
-            frameCount++;
-
-            if (frameCount > frameDelay) {
-                frameCount = 0;
-                currentFrame += animationDirection;
-
-                if (currentFrame > totalFrames - 1) {
-                    currentFrame = 0;
-                }
-                else if (currentFrame < 0) {
-                    currentFrame = totalFrames - 1;
-                }
-            }
-        }
-
-    }
-
-    public Animations walkLeft = new Animations(Images.lefttWalkDog1, 10);
-	public Animations jumpLeft = new Animations(Images.leftJumpDog1, 10);
-	public Animations idleLeft = new Animations(Images.leftIdleDog1, 10);
-
-	public Animations walkRight = new Animations(Images.rightWalkDog1, 10);
-	public Animations jumpRight = new Animations(Images.rightJumpDog1, 10);
-	public Animations idleRight = new Animations(Images.rightIdleDog1, 10);
-
-	public Animations move = idleRight;
+	
+	public static String direction = "right";
+	
+//	public void draw(Graphics2D g2) {
+//		BufferedImage img = null;
+//		if (direction.equals("right")) {
+//			if (jump!) {
+//				for (int i = 0; i < rightWalkDog1.length; i++) {
+//					g2.draw(rightWalkDog1[i], main.x, main.y, 40, 40);
+//				}
+//			}
+//		}
+//	}
+	// idk what im doing
 }
--- src/Character.java
+++ src/Character.java
@@ -45,15 +45,21 @@ public class Character extends Rectangle {
 	}
 
 	// basic mechanics are down
-
+	// retrieves current direction
+	// value -1 to 1
 	public double getDirection() {
 		return direction;
 	}
-
+	
+	// sets direction to specific value
+	// default 0
 	public void setDirection(int num) {
 		direction = num;
 	}
-
+	
+	// magnet effect on character atm will be fixed*
+	// character will fall if there is no block below it
+	// character will be set on the ground if there is a ground tile below it
 	public void fall() {
 		if (checkBelow() && !isJumping()) {
 			this.setLocation((int) this.getX(), tileY * Main.tileSize);
@@ -67,17 +73,20 @@ public class Character extends Rectangle {
 		} 
 	}
 
-	// check if character is jumping
+	// check if character is jumping based on true or false
 	public boolean isJumping() {
 		return jump;
 	}
 
-	// set to jumping or not jumping
+	// set to jumping or not jumping based on true or false
 	public void setJumping(boolean bool) {
 		jump = bool;
 	}
 
-	// change the character's y to simulate a jump
+	// change the character's y to simulate a jump if:
+	// - it is supposed to be jumping
+	// - direction is set to jumping
+	// - there is no block above it
 	public void jump() {
 		if (isJumping() && direction < 0) {
 			if (checkAbove()) {
@@ -102,6 +111,10 @@ public class Character extends Rectangle {
 		left = bool;
 	}
 
+	// move left if:
+	// - it is supposed to be moving left
+	// - it is not moving right
+	// - there is nothing blocking it left
 	public void moveLeft() {
 		if (this.isLeft() && !this.isRight()) {
 			if (checkLeft()) {
@@ -120,26 +133,29 @@ public class Character extends Rectangle {
 	public void setRight(boolean bool) {
 		right = bool;
 	}
-
+	// move right if:
+	// - it is supposed to be moving right
+	// - it is not moving left
+	// - there is nothing blocking it right
 	public void moveRight() {
 		if (this.isRight() && !this.isLeft()) {
 			if (checkRight()) {
 				this.setLocation(tileX * Main.tileSize, (int) this.getY());
 			} else {
 				this.translate(moveX, 0);
+				// screen scrolls if the character is moving right:
+				// - if it is not the last screen
+				// - if it is gonna move past the middle of the screen
+				// keeps the character in the middle of the screen until the last screen
 				if (Main.bgX > -(Main.levelWidth - Main.winWidth)
 						&& this.getX() >= Main.winWidth / 2 - Main.tileSize / 2) {
 					Main.bgX -= moveX;
-					// this.setLocation(Main.winWidth / 2 - Main.tileSize / 2, (int) this.getY());
+					this.setLocation(Main.winWidth / 2 - Main.tileSize / 2, (int) this.getY());
 				}
 			}
 		}
 	}
 
-	// returns if there is a block below or if it is at the bottom
-	// true if there is block
-	// true if at the bottom of frame
-	// false if there is no block
 	public void fixPosition() {
 		if (checkAbove() && isJumping()) {
 			this.setLocation((int) this.getX(), tileY*Main.tileSize);
@@ -156,21 +172,28 @@ public class Character extends Rectangle {
 			this.setLocation(tileX * Main.tileSize, (int) this.getY());
 		}
 	}
-
+	// returns if there is a block above or its at the ceiling
+	// true if there is a block
+	// true if at ceiling
+	// false if no block
 	public boolean checkAbove() {
 		if (tileY - 1 >= 0) {
 			return (Main.levelGrid[tileY - 1][tileX] == 1) ? true : false;
 		}
 		return true;
 	}
-
+	// returns if there is a block below or if it is at the bottom
+	// true if there is block
+	// true if at the bottom of frame
+	// false if there is no block
 	public boolean checkBelow() {
 		if (tileY + 1 < Main.tileHeight) {
 			return (Main.levelGrid[tileY + 1][tileX] == 1) ? true : false;
 		}
 		return true;
 	}
-
+	
+	// same idea
 	public boolean checkRight() {
 		if (tileX + 1 < Main.tileWidth) {
 			return (Main.levelGrid[tileY][tileX + 1] == 1) ? true : false;
